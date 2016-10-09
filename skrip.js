23145040684326a4abc744048473ce29a08047ed
meoli <jerichodaroya@gmail.com>
2016-10-09 23:02:34 +0800

    First Upload

---
+++ skrip.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("art_1").style.display = "block";
+}
+function myFunction2() {
+    document.getElementById("art_2").style.display = "block";
+    document.getElementById("pie").style.display = "none";
+
+}
